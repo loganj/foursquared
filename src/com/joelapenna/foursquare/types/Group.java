25f5baf8262234243d6cb7f1457fd16e2a8caa8d
Joe LaPenna <joe@joelapenna.com>
2009-07-21 22:05:09 -0700

    Make Group Parcelable.
    committer: Joe LaPenna <joe@joelapenna.com>

--- src/com/joelapenna/foursquare/types/Group.java
+++ src/com/joelapenna/foursquare/types/Group.java
@@ -4,12 +4,15 @@
 
 package com.joelapenna.foursquare.types;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
 /**
  * @author Joe LaPenna (joe@joelapenna.com)
  */
-public class Group extends ArrayList<FoursquareType> implements FoursquareType {
+public class Group extends ArrayList<FoursquareType> implements Parcelable, FoursquareType {
 
     private static final long serialVersionUID = 1L;
 
@@ -23,4 +26,33 @@ public class Group extends ArrayList<FoursquareType> implements FoursquareType {
         return mType;
     }
 
+    /* For Parcelable */
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeList(this);
+        dest.writeString(this.mType);
+    }
+
+    public static final Parcelable.Creator<Group> CREATOR = new Parcelable.Creator<Group>() {
+
+        @Override
+        public Group createFromParcel(Parcel source) {
+            Group instance = new Group();
+            source.readList(instance, null);
+            instance.setType(source.readString());
+            return instance;
+        }
+
+        @Override
+        public Group[] newArray(int size) {
+            return new Group[size];
+        }
+
+    };
 }
