05909b603668f41b89fa5d2bb45beac5101d7453
Joe LaPenna <joe@joelapenna.com>
2009-07-21 22:02:28 -0700

    +Remove unused import
    committer: Joe LaPenna <joe@joelapenna.com>

--- src/com/joelapenna/foursquared/VenueMapActivity.java
+++ src/com/joelapenna/foursquared/VenueMapActivity.java
@@ -20,7 +20,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 /**
  * @author Joe LaPenna (joe@joelapenna.com)
