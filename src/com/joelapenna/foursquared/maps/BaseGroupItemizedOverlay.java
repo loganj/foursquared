4bfbd550869e6bcdc5c0bbf352b0bfbd4c1d9958
Joe LaPenna <joe@joelapenna.com>
2009-07-21 22:05:07 -0700

    + Abstract an Group based ItemizedOverlay.
    committer: Joe LaPenna <joe@joelapenna.com>

---
+++ src/com/joelapenna/foursquared/maps/BaseGroupItemizedOverlay.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2009 Joe LaPenna
+ */
+
+package com.joelapenna.foursquared.maps;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
+import com.joelapenna.foursquare.types.Group;
+import com.joelapenna.foursquared.Foursquared;
+
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+/**
+ * @author Joe LaPenna (joe@joelapenna.com)
+ */
+abstract class BaseGroupItemizedOverlay extends ItemizedOverlay<OverlayItem> {
+    public static final String TAG = "BaseGroupItemizedOverlay";
+    public static final boolean DEBUG = Foursquared.DEBUG;
+
+    Group group = null;
+
+    public BaseGroupItemizedOverlay(Drawable defaultMarker) {
+        super(boundCenterBottom(defaultMarker));
+    }
+
+    @Override
+    public int size() {
+        return group.size();
+    }
+
+    @Override
+    public boolean onTap(GeoPoint p, MapView mapView) {
+        if (DEBUG) Log.d(TAG, "onTap: " + p);
+        mapView.getController().animateTo(p);
+        return super.onTap(p, mapView);
+    }
+
+    @Override
+    protected boolean onTap(int i) {
+        if (DEBUG) Log.d(TAG, "onTap: " + i);
+        return super.onTap(i);
+    }
+
+    public void setGroup(Group g) {
+        group = g;
+        super.populate();
+    }
+}
--- src/com/joelapenna/foursquared/maps/VenueItemizedOverlay.java
+++ src/com/joelapenna/foursquared/maps/VenueItemizedOverlay.java
@@ -5,7 +5,6 @@
 package com.joelapenna.foursquared.maps;
 
 import com.google.android.maps.GeoPoint;
-import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 import com.joelapenna.foursquare.types.Venue;
@@ -14,24 +13,20 @@ import com.joelapenna.foursquared.Foursquared;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 
-import java.util.ArrayList;
-
 /**
  * @author Joe LaPenna (joe@joelapenna.com)
  */
-public class VenueItemizedOverlay extends ItemizedOverlay<OverlayItem> {
+public class VenueItemizedOverlay extends BaseGroupItemizedOverlay {
     public static final String TAG = "VenueItemizedOverlay";
     public static final boolean DEBUG = Foursquared.DEBUG;
 
-    private ArrayList<Venue> mVenues = new ArrayList<Venue>();
-
     public VenueItemizedOverlay(Drawable defaultMarker) {
-        super(boundCenterBottom(defaultMarker));
+        super(defaultMarker);
     }
 
     @Override
     protected OverlayItem createItem(int i) {
-        Venue venue = mVenues.get(i);
+        Venue venue = (Venue)group.get(i);
         if (DEBUG) Log.d(TAG, "creating venue overlayItem: " + venue.getVenuename());
         int lat = (int)(Double.parseDouble(venue.getGeolat()) * 1E6);
         int lng = (int)(Double.parseDouble(venue.getGeolong()) * 1E6);
@@ -40,34 +35,10 @@ public class VenueItemizedOverlay extends ItemizedOverlay<OverlayItem> {
     }
 
     @Override
-    public int size() {
-        return mVenues.size();
-    }
-
-    @Override
     public boolean onTap(GeoPoint p, MapView mapView) {
         if (DEBUG) Log.d(TAG, "onTap: " + p);
         mapView.getController().animateTo(p);
         return super.onTap(p, mapView);
-    }
-
-    @Override
-    protected boolean onTap(int i) {
-        if (DEBUG) Log.d(TAG, "onTap: " + i);
-        return super.onTap(i);
-    }
-
-    public void addVenue(Venue venue) {
-        mVenues.add(venue);
-    }
-
-    /*
-     * We don't call populate every time we add a venue because that causes createItem to be called
-     * for every item already in the list. The documentation says that populate() will cache these
-     * calls to createItem but that does not seem to be the case.
-     */
-    public void finish() {
-        super.populate();
     }
 
     public static class VenueOverlayItem extends OverlayItem {
