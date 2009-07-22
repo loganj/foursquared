510e99edbd7fe9a9d1cf2f6c79313a2ec45edb4a
Joe LaPenna <jlapenna@zeke.dsf.cc>
2009-07-21 22:05:05 -0700

    + Testing having cool little windows pop up on maps markers. Doesn't work.
    committer: Joe LaPenna <joe@joelapenna.com>

--- src/com/joelapenna/foursquared/SearchVenueMapActivity.java
+++ src/com/joelapenna/foursquared/SearchVenueMapActivity.java
@@ -9,12 +9,22 @@ import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MyLocationOverlay;
+import com.google.android.maps.OverlayItem;
 import com.joelapenna.foursquare.types.Group;
 import com.joelapenna.foursquare.types.Venue;
 import com.joelapenna.foursquared.maps.VenueItemizedOverlay;
 
+import android.content.Context;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -118,8 +128,8 @@ public class SearchVenueMapActivity extends MapActivity {
 
     private void clearMap() {
         mMapView.getOverlays().remove(mVenuesOverlay);
-        mVenuesOverlay = new VenueItemizedOverlay(this.getResources()
-                .getDrawable(R.drawable.reddot));
+        mVenuesOverlay = new VenueItemizedOverlay(this.getResources().getDrawable(
+                R.drawable.reddot));
     }
 
     private boolean isVenueMappable(Venue venue) {
@@ -152,5 +162,47 @@ public class SearchVenueMapActivity extends MapActivity {
     @Override
     protected boolean isRouteDisplayed() {
         return false;
+    }
+
+    public class TestingVenueItemizedOverlay extends
+            com.joelapenna.foursquared.maps.VenueItemizedOverlay {
+
+        private PopupWindow mWindow;
+
+        public TestingVenueItemizedOverlay(Drawable defaultMarker) {
+            super(defaultMarker);
+        }
+
+        protected boolean onTap(int i) {
+            OverlayItem item = getItem(i);
+            Toast.makeText(getApplication(), item.getTitle(), Toast.LENGTH_SHORT).show();
+            PopupWindow window = getPopupWindow(item);
+            setPopupWindow(item);
+            return true;
+        }
+
+        private void setPopupWindow(OverlayItem item) {
+            GeoPoint geoPoint = item.getPoint();
+            Point point = new Point();
+            mMapView.getProjection().toPixels(geoPoint, point);
+            int x = point.x + mMapView.getLeft();
+            int y = point.y + 62; // TabHost height.
+            mWindow.showAtLocation(mMapView, Gravity.NO_GRAVITY, x, y);
+        }
+
+        private PopupWindow getPopupWindow(OverlayItem item) {
+            if (mWindow == null) {
+                LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View view = inflater.inflate(R.layout.venue_list_item, null);
+                mWindow = new PopupWindow(view, 128, 128, true);
+                mWindow.setBackgroundDrawable(getResources().getDrawable(R.drawable.icon));
+            }
+            TextView name = (TextView)mWindow.getContentView().findViewById(R.id.name);
+            name.setText(item.getTitle());
+
+            TextView locationLine1 = (TextView)mWindow.getContentView().findViewById(R.id.name);
+            locationLine1.setText(item.getSnippet());
+            return mWindow;
+        }
     }
 }
