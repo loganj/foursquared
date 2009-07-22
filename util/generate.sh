7d148ed37964edd80a5989ca73df154561bbb6ed
Joe LaPenna <joe@joelapenna.com>
2009-07-21 22:05:11 -0700

    Generate src/com/joelapenna/foursquare/{types,parsers} from captures/types
    committer: Joe LaPenna <joe@joelapenna.com>

---
+++ util/generate.sh
@@ -0,0 +1,8 @@
+for file in ../captures/types/*; do
+  base=$(basename $file .xml);
+  base=$(echo -n ${base:0:1}|tr '[:lower:]' '[:upper:]'; echo -n ${base:1});
+  echo "./gen_parser.py $file > ../src/com/joelapenna/foursquare/parsers/${base}Parser.java";
+  ./gen_parser.py $file > ../src/com/joelapenna/foursquare/parsers/${base}Parser.java;
+  echo "./gen_class.py $file > ../src/com/joelapenna/foursquare/types/${base}.java";
+  ./gen_class.py $file > ../src/com/joelapenna/foursquare/types/${base}.java;
+done;
