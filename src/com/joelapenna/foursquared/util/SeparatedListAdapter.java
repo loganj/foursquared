135378df9a8cf5456c24edf23ed86e3e65d71187
Joe LaPenna <joe@joelapenna.com>
2009-07-21 22:02:32 -0700

    +Use original ArrayAdapter constructor from jsharkey's demo code.
    committer: Joe LaPenna <joe@joelapenna.com>

--- src/com/joelapenna/foursquared/util/SeparatedListAdapter.java
+++ src/com/joelapenna/foursquared/util/SeparatedListAdapter.java
@@ -25,7 +25,7 @@ public class SeparatedListAdapter extends BaseAdapter {
     public final static int TYPE_SECTION_HEADER = 0;
 
     public SeparatedListAdapter(Context context) {
-        headers = new ArrayAdapter<String>(context, R.layout.list_header, R.id.list_header_title);
+        headers = new ArrayAdapter<String>(context, R.layout.list_header);
     }
 
     public void addSection(String section, Adapter adapter) {
