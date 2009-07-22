0f8db1a3e1a5fe567f88e87c2c0d4258b901814b
Joe LaPenna <joe@joelapenna.com>
2009-07-21 22:05:17 -0700

    remove unused debug constants
    committer: Joe LaPenna <joe@joelapenna.com>

--- src/com/joelapenna/foursquared/widget/BaseBadgeAdapter.java
+++ src/com/joelapenna/foursquared/widget/BaseBadgeAdapter.java
@@ -1,21 +1,18 @@
 /**
  * Copyright 2009 Joe LaPenna
  */
+
 package com.joelapenna.foursquared.widget;
 
 import com.joelapenna.foursquare.types.Badge;
 import com.joelapenna.foursquare.types.Group;
-import com.joelapenna.foursquared.Foursquared;
 
 import android.content.Context;
 
 /**
  * @author Joe LaPenna (joe@joelapenna.com)
- *
  */
 abstract public class BaseBadgeAdapter extends BaseGroupAdapter<Badge> {
-    private static final String TAG = "BaseTipAdapter";
-    private static final boolean DEBUG = Foursquared.DEBUG;
 
     public BaseBadgeAdapter(Context context, Group badges) {
         super(context, badges);
