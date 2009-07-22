89795ac68964ccddc128908ba609a76080b69012
Joe LaPenna <joe@joelapenna.com>
2009-07-21 22:05:11 -0700

    Add HttpApi that oauth-signs http requests.
    committer: Joe LaPenna <joe@joelapenna.com>

---
+++ src/com/joelapenna/foursquare/http/HttpApiWithOAuth.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2009 Joe LaPenna
+ */
+
+package com.joelapenna.foursquare.http;
+
+import com.joelapenna.foursquare.Foursquare;
+
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.exception.OAuthMessageSignerException;
+import oauth.signpost.impl.DefaultOAuthConsumer;
+import oauth.signpost.signature.SignatureMethod;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
+/**
+ * @author Joe LaPenna (joe@joelapenna.com)
+ */
+public class HttpApiWithOAuth extends HttpApi {
+    protected static final String TAG = "HttpApiWithOAuth";
+    protected static final boolean DEBUG = Foursquare.DEBUG;
+
+    private OAuthConsumer mConsumer;
+
+    public HttpApiWithOAuth(DefaultHttpClient httpClient) {
+        super(httpClient);
+    }
+
+    @Override
+    public HttpPost createHttpPost(String url, NameValuePair... nameValuePairs) {
+        HttpPost httpPost = super.createHttpPost(url, nameValuePairs);
+        try {
+            mConsumer.sign(httpPost);
+        } catch (OAuthMessageSignerException e) {
+            if (DEBUG) Log.d(TAG, "OAuthMessageSignerException", e);
+            throw new RuntimeException(e);
+        }
+        return httpPost;
+    }
+
+    public void setOAuthConsumerCredentials(String key, String secret) {
+        mConsumer = new DefaultOAuthConsumer(key, secret, SignatureMethod.HMAC_SHA1);
+    }
+
+    public void setOAuthTokenWithSecret(String token, String secret) {
+        mConsumer.setTokenWithSecret(token, secret);
+    }
+
+    public boolean hasOAuthTokenWithSecret() {
+        return (mConsumer.getToken() != null) && (mConsumer.getConsumerSecret() != null);
+    }
+}
---
+++ src/com/joelapenna/foursquare/test/HttpApiWithOAuthTestCase.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2009 Joe LaPenna
+ */
+
+package com.joelapenna.foursquare.test;
+
+import com.joelapenna.foursquare.http.HttpApiWithOAuth;
+
+import org.apache.http.Header;
+import org.apache.http.client.methods.HttpPost;
+
+import android.test.suitebuilder.annotation.LargeTest;
+
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Joe LaPenna (joe@joelapenna.com)
+ */
+public class HttpApiWithOAuthTestCase extends TestCase {
+
+    public HttpApiWithOAuthTestCase() {
+        super();
+    }
+
+    @LargeTest
+    public void test_createHttpPost_SignsRequest() throws IOException {
+        HttpApiWithOAuth httpApi = new HttpApiWithOAuth(HttpApiWithOAuth.createHttpClient());
+        httpApi.setOAuthConsumerCredentials(TestCredentials.oAuthConsumerKey,
+                TestCredentials.oAuthConsumerSecret);
+
+        HttpPost request = httpApi.createHttpPost("http://someurl");
+        Header authHeader = request.getFirstHeader("Authorization");
+        assertNotNull(authHeader);
+
+        String oauthHeader = authHeader.getValue();
+        assertTrue(oauthHeader.startsWith("OAuth "));
+    }
+}
