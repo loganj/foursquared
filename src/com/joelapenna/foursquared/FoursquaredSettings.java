fa4137b50427f91bfdb8e9b527f906694c9fb866
Joe LaPenna <joe@joelapenna.com>
2009-07-21 22:05:19 -0700

    Cut a release: 2009070300
    committer: Joe LaPenna <joe@joelapenna.com>

--- src/com/joelapenna/foursquared/FoursquaredSettings.java
+++ src/com/joelapenna/foursquared/FoursquaredSettings.java
@@ -13,7 +13,7 @@ public class FoursquaredSettings {
     public static final boolean API_DEBUG = false;
     public static final boolean PARSER_DEBUG = false;
 
-    public static final boolean USE_DUMPCATCHER = false;
+    public static final boolean USE_DUMPCATCHER = true;
     public static final boolean DUMPCATCHER_TEST = false;
 
 }
