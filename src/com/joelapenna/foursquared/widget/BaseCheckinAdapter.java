4b6934d60aee7549026c54031312bb1194693ba3
jlapenna <jlapenna@localhost>
2009-07-21 22:05:06 -0700

    Better abstract a group list adapter (like an array adapter but based on a foursquare group)
    committer: Joe LaPenna <joe@joelapenna.com>

--- src/com/joelapenna/foursquared/widget/BaseCheckinAdapter.java
+++ src/com/joelapenna/foursquared/widget/BaseCheckinAdapter.java
@@ -10,47 +10,21 @@ import com.joelapenna.foursquared.Foursquared;
 
 import android.content.Context;
 import android.util.Log;
-import android.widget.BaseAdapter;
 
 /**
  * @author Joe LaPenna (joe@joelapenna.com)
  */
-public abstract class BaseCheckinAdapter extends BaseAdapter {
-    private static final String TAG = "CheckinsListAdapter";
+public abstract class BaseCheckinAdapter extends BaseGroupAdapter<Checkin> {
+    private static final String TAG = "BaseCheckinAdapter";
     private static final boolean DEBUG = Foursquared.DEBUG;
 
-    private Group mCheckins;
-
-    public BaseCheckinAdapter(Context context, Group checkins) {
-        mCheckins = checkins;
-    }
-
-    @Override
-    public int getCount() {
-        if (DEBUG) Log.d(TAG, "getCount called.");
-        return mCheckins.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        if (DEBUG) Log.d(TAG, "getItem() called: " + String.valueOf(position));
-        return mCheckins.get(position);
+    public BaseCheckinAdapter(Context context, Group g) {
+        super(context, g);
     }
 
     @Override
     public long getItemId(int position) {
         if (DEBUG) Log.d(TAG, "getItemId() called: " + String.valueOf(position));
-        return Long.valueOf(((Checkin)mCheckins.get(position)).getCheckinid());
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return true;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        if (DEBUG) Log.d(TAG, "isEmpty() called");
-        return mCheckins.isEmpty();
+        return Long.valueOf(((Checkin)group.get(position)).getCheckinid());
     }
 }
--- src/com/joelapenna/foursquared/widget/BaseVenueAdapter.java
+++ src/com/joelapenna/foursquared/widget/BaseVenueAdapter.java
@@ -10,47 +10,21 @@ import com.joelapenna.foursquared.Foursquared;
 
 import android.content.Context;
 import android.util.Log;
-import android.widget.BaseAdapter;
 
 /**
  * @author Joe LaPenna (joe@joelapenna.com)
  */
-abstract public class BaseVenueAdapter extends BaseAdapter {
+abstract public class BaseVenueAdapter extends BaseGroupAdapter<Venue> {
     private static final String TAG = "BaseVenueAdapter";
     private static final boolean DEBUG = Foursquared.DEBUG;
 
-    private Group mVenues;
-
-    public BaseVenueAdapter(Context context, Group venues) {
-        mVenues = venues;
-    }
-
-    @Override
-    public int getCount() {
-        if (DEBUG) Log.d(TAG, "getCount() called");
-        return mVenues.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        if (DEBUG) Log.d(TAG, "getItem() called: " + String.valueOf(position));
-        return mVenues.get(position);
+    public BaseVenueAdapter(Context context, Group group) {
+        super(context, group);
     }
 
     @Override
     public long getItemId(int position) {
         if (DEBUG) Log.d(TAG, "getItemId() called: " + String.valueOf(position));
-        return Long.valueOf(((Venue)mVenues.get(position)).getVenueid());
-    }
-
-    @Override
-    public boolean hasStableIds() {
-        return true;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        if (DEBUG) Log.d(TAG, "isEmpty() called");
-        return mVenues.isEmpty();
+        return Long.valueOf(((Venue)group.get(position)).getVenueid());
     }
 }
