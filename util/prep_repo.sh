cbc09e1427be55ae63785dc770154856ccbf3df6
Joe LaPenna <joe@joelapenna.com>
2009-07-21 22:05:20 -0700

    Fix prep_repo script to support new repo layout.
    committer: Joe LaPenna <joe@joelapenna.com>

--- util/prep_repo.sh
+++ util/prep_repo.sh
@@ -6,19 +6,26 @@ existing=$1
 new=$2
 echo 'From: ' $existing ' to: ' $new
 
-path='res/values/credentials.xml'
+path='main/res/values/credentials.xml'
 existing_path=$existing/$path
 new_path=$new/$path
 echo "cp $existing_path $new_path"
 cp $existing_path $new_path
 
-path='tests/com/joelapenna/foursquare/TestCredentials.java'
+path='tests/src/com/joelapenna/foursquare/TestCredentials.java'
 existing_path=$existing/$path
 new_path=$new/$path
 echo "cp $existing_path $new_path"
 cp $existing_path $new_path
 
-path='.project'
+path='main/.project'
+existing_path=$existing/$path
+new_path=$new/$path
+echo "cp $existing_path $new_path"
+cp $existing_path $new_path
+sed -i "s#<name>foursquared</name>#<name>$new</name>#g" $new_path
+
+path='tests/.project'
 existing_path=$existing/$path
 new_path=$new/$path
 echo "cp $existing_path $new_path"
