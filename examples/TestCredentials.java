f2a2cd7582ad7ff4681f7697414225123b037416
Joe LaPenna <joe@joelapenna.com>
2009-07-22 21:30:04 -0700

    Add example credentials files.

---
+++ examples/TestCredentials.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2009 Joe LaPenna
+ */
+
+package com.joelapenna.foursquare;
+
+public class TestCredentials {
+
+    public static final String oAuthConsumerKey = "";
+    public static final String oAuthConsumerSecret = "";
+
+    public static final String oAuthToken = "";
+    public static final String oAuthTokenSecret = "";
+
+    public static final String testFoursquarePhone = "";
+    public static final String testFoursquarePassword = "";
+}
