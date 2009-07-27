848b05deb5b9fcddb8bbf9115506ce11ee53a9d2
Joe LaPenna <joe@joelapenna.com>
2009-07-28 08:28:15 +0900

    Remove unused icons and their iterator.

--- main/src/com/joelapenna/foursquared/util/IconsIterator.java
+++ main/src/com/joelapenna/foursquared/util/IconsIterator.java
@@ -21,16 +21,6 @@ public class IconsIterator {
         R.drawable.map_marker_purple_muted, //
     };
 
-    private static final Integer[] ALL_ICONS = {
-            R.drawable.reddot, //
-            R.drawable.bluedot, //
-            R.drawable.greendot, //
-            R.drawable.pinkdot, //
-            R.drawable.ltbluedot, //
-            R.drawable.yellowdot,
-    };
-
-    public final InfiniteIterator<Integer> allIcons = new InfiniteIterator<Integer>(ALL_ICONS);
     public final InfiniteIterator<Integer> icons = new InfiniteIterator<Integer>(MAP_NEW_ICONS);
     public final InfiniteIterator<Integer> beenthereIcons = new InfiniteIterator<Integer>(
             MAP_BEEN_THERE_ICONS);
