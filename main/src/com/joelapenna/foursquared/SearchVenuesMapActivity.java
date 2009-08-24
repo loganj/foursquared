7b777e225650c8761825b706d2cf312ff63a5680
Joe LaPenna <joe@joelapenna.com>
2009-08-23 19:35:40 -0700

    Make sure SearchVenuesMapActivity uses SearchVenuesActivity not NearbyVenuesActivity for its search results set. Fixes issue 49

--- main/src/com/joelapenna/foursquared/SearchVenuesMapActivity.java
+++ main/src/com/joelapenna/foursquared/SearchVenuesMapActivity.java
@@ -68,7 +68,7 @@ public class SearchVenuesMapActivity extends MapActivity {
             public void update(Observable observable, Object data) {
                 if (DEBUG) Log.d(TAG, "Observed search results change.");
                 clearMap();
-                loadSearchResults(NearbyVenuesActivity.searchResultsObservable.getSearchResults());
+                loadSearchResults(SearchVenuesActivity.searchResultsObservable.getSearchResults());
                 recenterMap();
             }
         };
@@ -82,10 +82,10 @@ public class SearchVenuesMapActivity extends MapActivity {
         // mMyLocationOverlay.enableCompass(); // Disabled due to a sdk 1.5 emulator bug
 
         clearMap();
-        loadSearchResults(NearbyVenuesActivity.searchResultsObservable.getSearchResults());
+        loadSearchResults(SearchVenuesActivity.searchResultsObservable.getSearchResults());
         recenterMap();
 
-        NearbyVenuesActivity.searchResultsObservable.addObserver(mSearchResultsObserver);
+        SearchVenuesActivity.searchResultsObservable.addObserver(mSearchResultsObserver);
     }
 
     @Override
@@ -94,7 +94,7 @@ public class SearchVenuesMapActivity extends MapActivity {
         if (DEBUG) Log.d(TAG, "onPause()");
         mMyLocationOverlay.disableMyLocation();
         mMyLocationOverlay.disableCompass();
-        NearbyVenuesActivity.searchResultsObservable.deleteObserver(mSearchResultsObserver);
+        SearchVenuesActivity.searchResultsObservable.deleteObserver(mSearchResultsObserver);
     }
 
     @Override
@@ -184,7 +184,7 @@ public class SearchVenuesMapActivity extends MapActivity {
     private void recenterMap() {
         GeoPoint center = mMyLocationOverlay.getMyLocation();
         if (center != null
-                && NearbyVenuesActivity.searchResultsObservable.getQuery() == NearbyVenuesActivity.QUERY_NEARBY) {
+                && SearchVenuesActivity.searchResultsObservable.getQuery() == SearchVenuesActivity.QUERY_NEARBY) {
             if (DEBUG) Log.d(TAG, "recenterMap via MyLocation as we are doing a nearby search");
             mMapController.animateTo(center);
             mMapController.setZoom(16);
