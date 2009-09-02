e036178c671c3e04b3041b6f251425c479f54847
Joe LaPenna <joe@joelapenna.com>
2009-09-02 14:41:33 -0700

    Remove unused parcelable stuff in Group.

--- main/src/com/joelapenna/foursquare/types/Group.java
+++ main/src/com/joelapenna/foursquare/types/Group.java
@@ -4,15 +4,12 @@
 
 package com.joelapenna.foursquare.types;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import java.util.ArrayList;
 
 /**
  * @author Joe LaPenna (joe@joelapenna.com)
  */
-public class Group extends ArrayList<FoursquareType> implements Parcelable, FoursquareType {
+public class Group extends ArrayList<FoursquareType> implements FoursquareType {
 
     private static final long serialVersionUID = 1L;
 
@@ -25,34 +22,4 @@ public class Group extends ArrayList<FoursquareType> implements Parcelable, Four
     public String getType() {
         return mType;
     }
-
-    /* For Parcelable */
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeList(this);
-        dest.writeString(this.mType);
-    }
-
-    public static final Parcelable.Creator<Group> CREATOR = new Parcelable.Creator<Group>() {
-
-        @Override
-        public Group createFromParcel(Parcel source) {
-            Group instance = new Group();
-            source.readList(instance, null);
-            instance.setType(source.readString());
-            return instance;
-        }
-
-        @Override
-        public Group[] newArray(int size) {
-            return new Group[size];
-        }
-
-    };
 }
