9f9ad3261b7662ca0e26dbbd540b6f08dbdcf5cf
Joe LaPenna <joe@joelapenna.com>
2009-10-11 14:33:40 -0700

    Remove excess menu options from SearchVenuesActivity.

--- main/src/com/joelapenna/foursquared/SearchVenuesActivity.java
+++ main/src/com/joelapenna/foursquared/SearchVenuesActivity.java
@@ -63,11 +63,8 @@ public class SearchVenuesActivity extends TabActivity {
     private static final int MENU_REFRESH = 1;
     private static final int MENU_NEARBY = 2;
     private static final int MENU_ADD_VENUE = 3;
-    private static final int MENU_CHECKINS = 4;
-    private static final int MENU_MYINFO = 5;
 
     private static final int MENU_GROUP_SEARCH = 0;
-    private static final int MENU_GROUP_ACTIVITIES = 1;
 
     private LocationManager mLocationManager;
     private BestLocationListener mLocationListener;
@@ -169,15 +166,6 @@ public class SearchVenuesActivity extends TabActivity {
         menu.add(MENU_GROUP_SEARCH, MENU_REFRESH, Menu.NONE, R.string.refresh_label) //
                 .setIcon(R.drawable.ic_menu_refresh);
 
-        if (!mIsShortcutPicker) {
-            menu.add(Menu.NONE, MENU_ADD_VENUE, Menu.NONE, R.string.add_venue_label) //
-                    .setIcon(android.R.drawable.ic_menu_add);
-            menu.add(MENU_GROUP_ACTIVITIES, MENU_CHECKINS, Menu.NONE, R.string.checkins_label) //
-                    .setIcon(R.drawable.ic_menu_allfriends);
-            menu.add(MENU_GROUP_ACTIVITIES, MENU_MYINFO, Menu.NONE, R.string.myinfo_label) //
-                    .setIcon(R.drawable.ic_menu_myinfo);
-            Foursquared.addPreferencesToMenu(this, menu);
-        }
         return true;
     }
 
@@ -194,15 +182,9 @@ public class SearchVenuesActivity extends TabActivity {
                 executeSearchTask(mSearchHolder.query);
                 return true;
             case MENU_ADD_VENUE:
-                startActivity(new Intent(SearchVenuesActivity.this, AddVenueActivity.class));
-                return true;
-            case MENU_CHECKINS:
-                Intent intent = new Intent(SearchVenuesActivity.this, FriendsActivity.class);
-                intent.setAction(Intent.ACTION_SEARCH);
+                Intent intent = new Intent(SearchVenuesActivity.this, AddVenueActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
                 startActivity(intent);
-                return true;
-            case MENU_MYINFO:
-                startActivity(new Intent(SearchVenuesActivity.this, UserActivity.class));
                 return true;
         }
         return super.onOptionsItemSelected(item);
