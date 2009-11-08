f580f3de01295e7fd0b9bf400259f13af77ab2fc
Joe LaPenna <joe@joelapenna.com>
2009-11-08 14:37:08 -0800

    Remove "classic" API support.

--- util/classic/generate.sh
+++
@@ -1,8 +0,0 @@
-for file in ../../captures/types/classic/*; do
-  base=$(basename $file .xml);
-  base=$(echo -n ${base:0:1}|tr '[:lower:]' '[:upper:]'; echo -n ${base:1});
-  echo "./gen_parser.py $file > ../../src/com/joelapenna/foursquare/parsers/classic/${base}Parser.java";
-  ./gen_parser.py $file > ../../src/com/joelapenna/foursquare/parsers/classic/${base}Parser.java;
-  echo "./gen_class.py $file > ../../src/com/joelapenna/foursquare/types/classic/${base}.java";
-  ./gen_class.py $file > ../../src/com/joelapenna/foursquare/types/classic/${base}.java;
-done;
