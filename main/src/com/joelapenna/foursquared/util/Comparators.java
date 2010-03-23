1cd250c98a030101787a7e7d90f168497663dc58
markww@gmail.com <markww@gmail.com>
2010-03-23 13:03:22 -0700

    Updated comparator for Venue, was doing a string comparison for distances.

--- main/src/com/joelapenna/foursquared/util/Comparators.java
+++ main/src/com/joelapenna/foursquared/util/Comparators.java
@@ -31,7 +31,23 @@ public class Comparators {
                  */
                 @Override
                 public int compare(Venue object1, Venue object2) {
-                    return object1.getDistance().compareTo(object2.getDistance());
+                    // TODO: In practice we're pretty much guaranteed to get valid locations
+                    // from foursquare, but.. what if we don't, what's a good fail behavior
+                    // here?
+                    try {
+                        int d1 = Integer.parseInt(object1.getDistance());
+                        int d2 = Integer.parseInt(object2.getDistance());
+                        
+                        if (d1 < d2) {
+                            return -1;
+                        } else if (d1 > d2) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    } catch (NumberFormatException ex) {
+                        return object1.getDistance().compareTo(object2.getDistance());
+                    }
                 }
             };
         }
