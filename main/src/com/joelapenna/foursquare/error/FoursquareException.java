3ed219045ec64ce7f427d7cefc68e5259e46cc89
markww@gmail.com <markww@gmail.com>
2010-04-09 12:21:57 -0700

    Updated style of check-in button to be easier to hit and should be in line with the rest of the styles on the other platforms.

--- main/src/com/joelapenna/foursquare/error/FoursquareException.java
+++ main/src/com/joelapenna/foursquare/error/FoursquareException.java
@@ -9,9 +9,19 @@ package com.joelapenna.foursquare.error;
  */
 public class FoursquareException extends Exception {
     private static final long serialVersionUID = 1L;
+    
+    private String mExtra;
 
     public FoursquareException(String message) {
         super(message);
     }
 
+    public FoursquareException(String message, String extra) {
+        super(message);
+        mExtra = extra;
+    }
+    
+    public String getExtra() {
+        return mExtra;
+    }
 }
